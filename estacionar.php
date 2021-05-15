1561218123a126ff2e9a907c24a1aae54eed7dc7
unknown <socrams.88@gmail.com>
2021-05-14 21:50:20 -0300

    primeras pantallas

--- estacionar.php
+++ estacionar.php
@@ -13,7 +13,7 @@
     
 
     <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
 
