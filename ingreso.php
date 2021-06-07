da7d31d4f3dc0994afc5f22576a860f09b16dc90
unknown <socrams.88@gmail.com>
2021-06-07 20:17:03 -0300

    repare los links de ingreso.php

--- ingreso.php
+++ ingreso.php
@@ -1,15 +1,12 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <?php include_once "titulo.php";
-    include_once "stylo.php"; ?>
+    <?php include_once "titulo.php";?>
+
     <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/cover/">
     <!-- Bootstrap core CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-
-
-    <style>
+<style>
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -17,16 +14,14 @@
         -moz-user-select: none;
         -ms-user-select: none;
         user-select: none;
-      }
+}
 
       @media (min-width: 768px) {
         .bd-placeholder-img-lg {
           font-size: 3.5rem;
         }
       }
-    </style>
-
-    
+    </style>    
     <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
   </head>
@@ -38,7 +33,7 @@
       <h3 class="masthead-brand">Te Lo Estaciono</h3>
       <nav class="nav nav-masthead justify-content-center">
         <a class="nav-link active" href="\TpObligatorioEstacionamiento\login.php">Ingresar</a>
-        <a class="nav-link" href="\TpObligatorioEstacionamiento\index.php">Salir</a>
+        <a class="nav-link" href="index.php">Salir</a>
         <!--a class="nav-link" href="#"></a-->
       </nav>
     </div>
@@ -57,18 +52,16 @@ IngresarAuto($patente, $fechayhora);
     <h1 class="cover-heading">Correcto</h1>
     <p class="lead"><?php echo "ingreso patente numero ".$patente.", Hora ".$fechayhora; ?></p>
     <p class="lead">
-      <a href="\TpObligatorioEstacionamiento\estacionar.php" class="btn btn-lg btn-secondary">Volver</a>
+      <a href="estacionar.php" class="btn btn-lg btn-secondary">Volver</a>
     </p>
   </main>
 
   <footer class="mastfoot mt-auto">
     <div class="inner">
-      <p><a href="#">Te Lo estaciono</a>, by <a href="\TpObligatorioEstacionamiento\index.php">@Socrams</a>.</p>
+      <p><a href="#">Te Lo estaciono</a>, by <a href="index.php">@Socrams</a>.</p>
     </div>
   </footer>
 </div>
-
-
-    
+   
   </body>
 </html>
