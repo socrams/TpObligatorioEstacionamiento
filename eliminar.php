30fed99615c8538859e5804ccc8787bbdd2ce16e
unknown <socrams.88@gmail.com>
2021-06-25 17:22:48 -0300

    correccion error al eliminar online

--- eliminar.php
+++ eliminar.php
@@ -1,11 +1,10 @@
-<a href="Eliminar.php?name=img/<?php echo $archivos[$a]; ?>" style="color: red;">
+<a href="Eliminar.php?name=img/<?php echo $archivos[$i]; ?>" 
+style="color: red;">
     Eliminar
 </a>
-
 <?php
 // Usamos el comando "unlink" para borrar el fichero
 unlink($_GET["name"]);
-
 // Redirigiendo hacia atrás
 header("Location: estacionar.php")
 ?>
